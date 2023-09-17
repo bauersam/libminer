acc7ddf4c729afdbb9c4aa21df8232fe2d33e0e6
Samantha Bauer <sbauer@wayne.edu>
2023-09-17 17:13:35 -0400

    Add sizes param to lib_summary to calculate lib sizes.

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -2,17 +2,34 @@
 #'
 #' Provide a brief summary of the package libraries on your machine.
 #'
+#' @param sizes logical indicating whether or not to calculate
+#' library sizes. Default `FALSE`.
+#'
 #' @return A two-column `data.frame` containing the count of packages
 #' in each of the user's libraries.
 #' @export
 #'
 #' @examples
 #' lib_summary()
-lib_summary <- function() {
+lib_summary <- function(sizes = FALSE) {
+  if(!is.logical(sizes)) {
+    stop("'sizes' must be TRUE or FALSE.") #stop: exits function & give error
+  }
+
   #Tells us how many packages are in each library
   pkgs <- utils::installed.packages()
   pkg_tbl <- table(pkgs[, "LibPath"])
   pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
   names(pkg_df) <- c("Library", "n_packages")
+
+  if(sizes == TRUE) {
+    pkg_df$lib_size <-  sapply(
+      pkg_df$Library,
+      function(x) {
+        sum(fs::file_size(fs::dir_ls(x, recurse = TRUE)))
+      }
+    )
+  }
+
   pkg_df
 }
