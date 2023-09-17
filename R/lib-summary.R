6cf38a19e21643401b4368f21b93391f95dac12c
Samantha Bauer <fr8412@wayne.edu>
2023-09-17 14:47:04 -0400

    Documenting.

--- R/lib-summary.R
+++ R/lib-summary.R
@@ -1,3 +1,13 @@
+#' R Library Summary
+#'
+#' Provide a brief summary of the package libraries on your machine.
+#'
+#' @return A two-column `data.frame` containing the count of packages
+#' in each of the user's libraries.
+#' @export
+#'
+#' @examples
+#' lib_summary()
 lib_summary <- function() {
   #Tells us how many packages are in each library
   pkgs <- utils::installed.packages()
