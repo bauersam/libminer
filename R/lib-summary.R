c6c5d52fd59262c2017021e4b85600da80a58030
Samantha Bauer <fr8412@wayne.edu>
2023-09-17 12:24:31 -0400

    Initial commit

---
+++ R/lib-summary.R
@@ -0,0 +1,8 @@
+lib_summary <- function() {
+  #Tells us how many packages are in each library
+  pkgs <- utils::installed.packages()
+  pkg_tbl <- table(pkgs[, "LibPath"])
+  pkg_df <- as.data.frame(pkg_tbl, stringsAsFactors = FALSE)
+  names(pkg_df) <- c("Library", "n_packages")
+  pkg_df
+}
